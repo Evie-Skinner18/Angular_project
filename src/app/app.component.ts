a1f7b683cbe3cfdeaec12e3716010594027ce31a
Evie Skinner <eves@evies-mbp.home>
2018-07-21 15:23:18 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// metadata which connects app-root to custom html tags in index.html
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'FightCLUB';
 }
